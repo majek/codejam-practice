117f597c8c02985344950196ce6d0bcb1f50a495
Marek Majkowski <majek04@gmail.com>
2013-04-18 21:35:23 +0100

    Cleaner, but still works only for small data set

--- 32003/D.py
+++ 32003/D.py
@@ -6,32 +6,34 @@ import itertools
 all_combinations = lambda a: itertools.chain(*(itertools.combinations(a, l)
                                                for l in range(1,len(a)+1)))
 
-
-Store = collections.namedtuple("Store", ["no", "x", "y", "has", "itemnames"])
+Store = collections.namedtuple("Store", ["x", "y", "cost"])
 
 def dist(a, b):
     return math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2)
 
-def solve(a, b, cost, shopping, unvisited_stores, to_buy):
-    # Visit b and buy to_buy
-    unvisited_stores = unvisited_stores - set((b,))
-    shopping = shopping - to_buy
-    cost += sum(stores[b].has[item] for item in to_buy) + distances[a][b]
-    if to_buy & perishable:
-        cost += distances[b][0]
-        b = 0
-
+def solve(a, shopping, whf):
     if not shopping:
-        return cost + distances[b][0]
+        return distances[a][0]
+    rmin = Ellipsis
+    for item in shopping:
+        to_buy = set((item,))
+        is_per = bool(to_buy & perishable)
+        rem_shopping = shopping - to_buy
 
-    rets = [Ellipsis]
-    for c in unvisited_stores:
-        to_buy = shopping & stores[c].itemnames
-        if not to_buy:
-            continue
-        for tb in all_combinations(list(to_buy)):
-            rets.append( solve(b, c, cost, shopping, unvisited_stores, set(tb)) )
-    return min(rets)
+        for s in shops_selling[item]:
+            cost = distances[a][s] + stores[s].cost[item]
+            whf2 = whf
+            if is_per:
+                if s not in whf:
+                    cost += distances[s][0]
+                    whf2 |= set((s,))
+                    s = 0
+                else:
+                    cost -= distances[a][s]
+                    s = a
+            r = cost + solve(s, rem_shopping, whf2)
+            rmin = min(rmin, r)
+    return rmin
 
 
 for case_no in xrange(0, input()):
@@ -41,29 +43,29 @@ for case_no in xrange(0, input()):
     _, num_stores, price_of_gas = map(int, raw_input().split())
     shopping = raw_input().split()
 
-    perishable = frozenset()
+    perishable = set()
     for i in xrange(len(shopping)):
         if shopping[i][-1] == '!':
             shopping[i] = shopping[i][:-1]
-            perishable = perishable | set([shopping[i]])
+            perishable |= set([shopping[i]])
 
-    shopping = frozenset(shopping)
-
-    stores = [Store(0, 0, 0, {}, set())]
+    stores = [Store(0, 0, {})]
+    shops_selling = collections.defaultdict(set)
     for i in range(1, num_stores+1):
         l = raw_input().split()
-        x_pos, y_pos = l[:2]
+        x_pos, y_pos = map(int, l[:2])
         has = {}
         for ip in l[2:]:
             item, price = ip.split(':')
             if item not in shopping: continue
             has[item] = int(price)
-        stores.append( Store(i, int(x_pos), int(y_pos), has, set(has.keys())) )
+            shops_selling[item].add( i )
+        stores.append( Store(x_pos, y_pos, has) )
 
     distances = [[None] * len(stores) for _ in range(len(stores))]
     for a in range(len(stores)):
         for b in range(a, len(stores)):
             distances[a][b] = distances[b][a] = \
                 dist(stores[a], stores[b]) * price_of_gas
-    c = solve(0, 0, 0.0, shopping, frozenset(range(1, len(stores))), set())
-    print '%.7f' % (c,)
+
+    print '%.7f' % (solve(0, frozenset(shopping), frozenset()),)
