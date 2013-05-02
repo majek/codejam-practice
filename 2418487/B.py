7fe22318acad3d6d9e4d13468bf2c0c49c5dc0a6
Marek Majkowski <majek04@gmail.com>
2013-05-02 05:23:15 +0100

    1A 2013 - B

--- 2418487/B.py
+++ 2418487/B.py
@@ -3,28 +3,45 @@ import itertools
 import sys
 import math
 
-def solve(e, n):
-    k = (e, n)
-    if k in mem: return mem[k]
 
-    if n == len(V):
-        return 0
+def suck(n):
+    spent = M[n] - T[n]
+    S[n] = spent
 
-    #r = [0 + solve(min(E, e+R), n+1), e*V[n] + solve(R, n+1)]
-    r = []
-    for s in xrange(0, e+1):
-        r.append( s*V[n] + solve(min(E, e-s+R), n+1) )
-    mem[k] = max(r)
-    return mem[k]
+    s = M[n] - spent
+    for r in xrange(n+1, len(M)):
+        s += R
+        if s >= E:
+            break
+        if M[r] > s:
+            M[r] = s
+        else:
+            # assert
+            break
+
+    s = M[n]
+    for r in xrange(n-1, 0-1, -1):
+        s -= R
+        if s <= 0:
+            break
+        if T[r] < s:
+            # I don't understand that
+            T[r] = s
 
 for case_no in xrange(1, input() + 1):
     print >> sys.stderr, "Case #%s:" % (case_no,)
     print "Case #%s:" % (case_no,),
 
-    E, R, _ = map(int, raw_input().split())
+    E, R, N = map(int, raw_input().split())
     V = map(int, raw_input().split())
 
-    # gain = v1 * spent
+    M = [E] * N   # Energy on entry
+    T = [0] * N   # Energy reserved, must be available on exit
+    S = [0] * N   # Spent
 
-    mem = {}
-    print solve(E, 0)
+    vp = sorted([(v, p) for p, v in enumerate(V)], reverse=True, key=lambda (a,b):(a, N-b))
+
+    for v, p in vp:
+        suck(p)
+
+    print sum(v*p for v,p in itertools.izip(V, S))
