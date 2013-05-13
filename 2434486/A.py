7971790f23e8769548b8913e865e780ac616365c
Marek Majkowski <majek04@gmail.com>
2013-05-13 11:16:45 +0100

    Round 1B 2013 - A

--- 2434486/A.py
+++ 2434486/A.py
@@ -19,8 +19,6 @@ def solve(A, idx):
         else:
             return solve(A, idx+1) + 1
 
-
-
 for case_no in xrange(1, input() + 1):
     print >> sys.stderr, "Case #%s:" % (case_no,)
     print "Case #%s:" % (case_no,),
