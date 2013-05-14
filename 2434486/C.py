b5bb0e3f22ff6016a4f7b145edf3c1e1c7f25707
Marek Majkowski <majek04@gmail.com>
2013-05-14 11:57:57 +0100

    Round 1B 2013 - C

--- 2434486/C.py
+++ 2434486/C.py
@@ -1,93 +1,69 @@
-#
-# This code is enough for small data set, but it's way too slow for
-# the large one.
-#
 import sys
-import os.path
-import cPickle as pickle
+sys.setrecursionlimit(10000)
 
 
-def _all_sub(word, s):
-    r = []
-    if not word:
-        return r
-    for i in xrange(s+5, len(word)):
-        w = word[:i] + '_' + word[i+1:]
-        r.append( (w, i) )
-    return r
-
-def all_sub(word):
-    yield word
-    a = _all_sub(word, -5)
-    while a:
-        x = []
-        for w, s in a:
-            yield w
-            x.extend(_all_sub(w, s))
-        a = x
-
-
-if not os.path.exists('garbled_email.pickle'):
-    with open("garbled_email_dictionary.txt") as fd:
-        Words = [w.strip() for w in fd.readlines()]
-
-    all_subw = set()
-    for word in Words:
-        all_subw.update( all_sub(word) )
-
-    big_tree = {}
-    for w in all_subw:
-        t = big_tree
-        for c in w:
-            t = t.setdefault(c, {})
-        t['#'] = True
-    with open('garbled_email.pickle', 'wb') as fd:
-        pickle.dump(big_tree, fd)
-else:
-    with open('garbled_email.pickle', 'rb') as fd:
-        big_tree = pickle.load(fd)
+prefix_tree = {}
+with open("garbled_email_dictionary.txt") as fd:
+    for word in fd.read().split():
+        tree = prefix_tree
+        for c in word:
+            tree = tree.setdefault(c, {})
+        tree['#'] = word
 
 print >> sys.stderr, "[*] Loaded!"
 
 
-def all_break2(word):
-    trees = [big_tree]
-    for c in word:
-        ntrees = []
-        wordend = False
-        for t in trees:
-            if c in t:
-                ntrees.append( t[c] )
-            if '#' in t:
-                wordend = True
-        if wordend and c in big_tree:
-            ntrees.append( big_tree[c] )
-        if not ntrees:
-            return False
-        trees = ntrees
-    return bool(sum(1 for t in trees if '#' in t))
+def _solve(msg, tree, change):
+    if not msg:
+        return (0 if '#' in tree else Ellipsis)
 
+    C = msg[0]
+    r = []
+    if change > 0:
+        # no changes
+        if C in tree:
+            r.append( (tree[C], change - 1, 0) )
+        if '#' in tree and C in prefix_tree:
+            r.append( (prefix_tree[C], change - 1, 0) )
+    else:
+        # changes possible
+        for c in tree:
+            if c != '#':
+                # letter
+                if c == C:
+                    r.append( (tree[c], 0, 0) )
+                else:
+                    r.append( (tree[c], 4, 1) )
+            else:
+                # new word
+                for d in prefix_tree:
+                    if d == C:
+                        r.append( (prefix_tree[d], 0, 0) )
+                    else:
+                        r.append( (prefix_tree[d], 4, 1) )
 
-def all_break(word):
-    if not word:
-        return True
-    for i in xrange(min(len(word)+1, 11), 1-1, -1):
-        if word[:i] not in all_subw:
-            continue
-        if all_break(word[i:]):
-            return True
-    return False
+    best = Ellipsis
+    nmsg = msg[1:]
+    for ntree, nchange, cost in r:
+        if best > cost:
+            x = solve(nmsg, ntree, nchange)
+            if x is not Ellipsis:
+                best = min(best, x + cost)
+    return best
+
+def solve(message, tree, change):
+    k = (message, id(tree), change)
+    if k not in mem:
+        mem[k] = _solve(message, tree, change)
+    return mem[k]
 
 
 for case_no in xrange(1, input() + 1):
+
     print >> sys.stderr, "Case #%s:" % (case_no,)
     print "Case #%s:" % (case_no,),
 
     S = raw_input()
 
-    for sw in all_sub(S):
-        if all_break2(sw):
-            print sum(1 for c in sw if c == '_')
-            break
-    else:
-        print '!'
+    mem = {}
+    print solve(S, prefix_tree, 0)
