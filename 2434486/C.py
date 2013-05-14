d67f098241e1bfbc34e33acbe4857ec4ece3f09c
Marek Majkowski <majek04@gmail.com>
2013-05-14 02:57:01 +0100

    Round 1B 2013 - C

--- 2434486/C.py
+++ 2434486/C.py
@@ -1,8 +1,6 @@
-import collections
-import itertools
 import sys
-import math
-
+import os.path
+import cPickle as pickle
 
 
 def _all_sub(word, s):
@@ -25,14 +23,45 @@ def all_sub(word):
         a = x
 
 
-with open("garbled_email_dictionary.txt") as fd:
-    Words = [w.strip() for w in fd.readlines()]
+if not os.path.exists('garbled_email.pickle'):
+    with open("garbled_email_dictionary.txt") as fd:
+        Words = [w.strip() for w in fd.readlines()]
 
-all_subw = set()
-for word in Words:
-    all_subw.update( all_sub(word) )
+    all_subw = set()
+    for word in Words:
+        all_subw.update( all_sub(word) )
+
+    big_tree = {}
+    for w in all_subw:
+        t = big_tree
+        for c in w:
+            t = t.setdefault(c, {})
+        t['#'] = True
+    with open('garbled_email.pickle', 'wb') as fd:
+        pickle.dump(big_tree, fd)
+else:
+    with open('garbled_email.pickle', 'rb') as fd:
+        big_tree = pickle.load(fd)
 
 print >> sys.stderr, "[*] Loaded!"
+
+
+def all_break2(word):
+    trees = [big_tree]
+    for c in word:
+        ntrees = []
+        wordend = False
+        for t in trees:
+            if c in t:
+                ntrees.append( t[c] )
+            if '#' in t:
+                wordend = True
+        if wordend and c in big_tree:
+            ntrees.append( big_tree[c] )
+        if not ntrees:
+            return False
+        trees = ntrees
+    return bool(sum(1 for t in trees if '#' in t))
 
 
 def all_break(word):
@@ -53,9 +82,8 @@ for case_no in xrange(1, input() + 1):
     S = raw_input()
 
     for sw in all_sub(S):
-        if all_break(sw):
+        if all_break2(sw):
             print sum(1 for c in sw if c == '_')
             break
-
     else:
-        assert 0
+        print '!'
