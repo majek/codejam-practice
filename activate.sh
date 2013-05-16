4b29bf86b913db3fc3871401848b2326a1c7a566
Marek Majkowski <majek04@gmail.com>
2013-05-16 12:51:36 +0100

    More tooling

--- activate.sh
+++ activate.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 # wget https://codejam-commandline.googlecode.com/files/codejam-commandline-1.2-beta1.zip
 
-echo "[*] New competition: gcj_init_contest.py XXXX"
+CONTEST=`cat $HOME/src/codejam-commandline-1.2-beta1/config/current_config.py |grep contest_id|cut -d "'" -f 4`
+
+
+echo "[*] Google CodeJAM. Current contest: $CONTEST"
+echo ""
+echo "    gcj_init_contest.py XXXX"
+echo "    gcj_renew_login.py"
 echo ""
 echo "    gcj_run A tiny"
-echo "    gcj_upload_the_answer A small"
+echo "    gcj_upload A small"
+echo "    gcj_upload_retry A small"
 echo "    gcj_get_status.py"
 echo ""
 echo "[ ] Docs: less $HOME/src/codejam-commandline-1.2-beta1/README"
 export PS1="(codejam) $PS1"
 export PATH="$HOME/codejam:$HOME/src/codejam-commandline-1.2-beta1:$PATH"
+
+#echo "[*] Renewing login:"
+#$HOME/src/codejam-commandline-1.2-beta1/gcj_renew_login.py > /dev/null
