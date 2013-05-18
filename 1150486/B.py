b0688389402ca2a57c676ea8009fc5787224ff38
Marek Majkowski <majek04@gmail.com>
2013-05-18 19:42:04 +0100

    Round 2 2011 - B (large)

--- 1150486/B.py
+++ 1150486/B.py
@@ -1,8 +1,5 @@
-import collections
 import itertools
 import sys
-import math
-from pprint import pprint as pp
 
 
 def transpose(A):
@@ -18,23 +15,24 @@ def sum_matrix(A, k):
     R = len(A); C = len(A[0])
     B = [[None] * (C-k+1) for _ in xrange(R-k+1)]
 
-    pr = 0
-    for c in xrange(0, C-k+1):
-        if c == 0:
-            ss = sum(sum(A[r][c:c+k]) for r in xrange(0, k-1))
-            pr = ss
-        else:
-            ss = pr + sum(-A[r][c-1] + A[r][c+k-1] for r in xrange(0, k-1))
-            pr = ss
-        for r in xrange(0, R-k+1):
-            ss += sum(A[r+k-1][c:c+k])
+    # Precompute sums of k consecutive items for every row
+    rsum = [[None] * (C-k+1) for _ in xrange(R)]
+    for r in xrange(R):
+        rsum[r][0] = p = sum(A[r][:k])
+        for c in xrange(1, C-k+1):
+            rsum[r][c] = p = p - A[r][c-1] + A[r][c+k-1]
+
+    for c in xrange(C-k+1):
+        ss = sum(rsum[r][c] for r in xrange(k-1))
+        for r in xrange(R-k+1):
+            ss += rsum[r+k-1][c]
             B[r][c] = ss - A[r][c] - A[r+k-1][c+k-1] - A[r+k-1][c] - A[r][c+k-1]
-            ss -= sum(A[r][c:c+k])
+            ss -= rsum[r][c]
     return B
 
 def pos_matrix(A, off):
     t = [c + off for c in xrange(len(A[0]))]
-    return [[v * c for c, v in itertools.izip(A[r], t)] for r in xrange(len(A))]
+    return [[v * c for c, v in itertools.izip(row, t)] for row in A]
 
 
 def solve(k):
@@ -57,9 +55,9 @@ for case_no in xrange(1, input() + 1):
     R, C, D = map(int, raw_input().split())
 
     W = [map(int, list(raw_input())) for _ in xrange(R)]
+
     X = W
     Y = transpose(W)
-
     pos_X = pos_matrix(X, 0)
     pos_Y = pos_matrix(Y, 0)
 
